83cc5afe23f8a00812f5dc9ad35e59892c35d334
Anne Risgaard Hostrup <annerishos@gmail.com>
2025-08-18 09:03:08 +0200

    Add project files.

---
+++ Nedarvning/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nedarvning
+{
+    internal class Vehicle
+    {
+        public string Brand;
+        public string MaxSpeed;
+    }
+}
